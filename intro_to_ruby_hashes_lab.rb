bb1bae370db67977daced7cdac7dc2e58f2c81e2
Stavroula Guerrero <4stacyg@optonline.net>
2018-06-21 21:01:51 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -36,10 +36,18 @@ def monopoly_with_third_tier
 			},
 			:names =>
 			{
-				:reading_railroad => {},
-				:pennsylvania_railroad => {},
-				:b_and_o_railroad => {},
-				:shortline => {},
+				:reading_railroad => {
+					"mortgage_value" => "$100"
+				},
+				:pennsylvania_railroad => {
+					"mortgage_value" => "$200"
+				},
+				:b_and_o_railroad => {
+					"mortgage_value" => "$400"
+				},
+				:shortline => {
+					"mortgage_value" => "$800"
+				},
 			},
 		}
 	}
